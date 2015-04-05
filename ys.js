515ea3423f604362c8347429625c698da0d432a8
YONGSIK SIM <ys4577@gmail.com>
2015-04-05 17:49:34 +0900

    v1.0.1.0(15040501) -added a function : String.prototype.cutStr

--- ys.js
+++ ys.js
@@ -1,4 +1,7 @@
-var ys.addComma = function (a)
-{
-	return a ? a.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") : null;
-};
+/* ys.js : this is a personal javascript function library */
+(function(){
+	"use strict";
+	String.prototype.addComma = function(){return this?this.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"):this;};
+	String.prototype.cutStr = function(a){return (this.indexOf(' ', a)==-1)?this:this.substring(0,this.indexOf(' ', a));}
+	String.prototype.cutStr = function(a,b){return (this.indexOf(' ', a)==-1)?this:this.substring(0,this.indexOf(' ', a))+(b?"...":"");}
+}());
